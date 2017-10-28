8725e1011c9557d125a008c1b6cdaed9d9257736
negebauer <negebauer@uc.cl>
2017-10-27 23:52:40 -0300

    vector init

--- Assignment2-PropertyTesting-master/src/main/java/tarea02/Vector.java
+++ Assignment2-PropertyTesting-master/src/main/java/tarea02/Vector.java
@@ -2,114 +2,120 @@ package tarea02;
 
 /**
  * A 3D Vector.
- * Since it uses Fractions, calculations are 
+ * Since it uses Fractions, calculations are
  * guaranteed to be exact
  * */
 public class Vector {
-	
-	public Vector(Fraction x, Fraction y, Fraction z) {
-		//TODO stub
-	}
 
-	/**
-	 * a + b
-	 * */
-	public static Vector add(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * a - b
-	 * */
-	public static Vector sub(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
+  Fraction x;
+  Fraction y;
+  Fraction z;
 
-	/**
-	 * (a.X * bX, a.Y * b.Y, a.Z * b.Z)
-	 * */
-	public static Vector mul(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * (a.X / bX, a.Y / b.Y, a.Z / b.Z)
-	 * */
-	public static Vector div(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * Multiplication of a vector by a scalar
-	 * */
-	public static Vector mulScalar(Vector a, Fraction s) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * Division of a vector by a scalar
-	 * */
-	public static Vector divScalar(Vector a, Fraction s) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * Dot product
-	 * */
-	public static Fraction dot(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * Cross product
-	 * */
-	public static Vector cross(Vector a, Vector b) {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * Zero vector
-	 * */
-	public static Vector zero() {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * (1, 0, 0)
-	 * */
-	public static Vector eX() {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * (0, 1, 0)
-	 * */
-	public static Vector eY() {
-		//TODO stub
-		return null;
-	}
-	
-	/**
-	 * (0, 0, 1)
-	 * */
-	public static Vector eZ() {
-		//TODO stub
-		return null;
-	}
-	
-	public static boolean equals(Vector a, Vector b) {
-		//TODO stub
-		return false;
-	}
+  public Vector(Fraction x, Fraction y, Fraction z) {
+    this.x=x;
+    this.y=y;
+    this.z=z;
+  }
+
+  /**
+   * a + b
+   * */
+  public static Vector add(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * a - b
+   * */
+  public static Vector sub(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * (a.X * bX, a.Y * b.Y, a.Z * b.Z)
+   * */
+  public static Vector mul(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * (a.X / bX, a.Y / b.Y, a.Z / b.Z)
+   * */
+  public static Vector div(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * Multiplication of a vector by a scalar
+   * */
+  public static Vector mulScalar(Vector a, Fraction s) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * Division of a vector by a scalar
+   * */
+  public static Vector divScalar(Vector a, Fraction s) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * Dot product
+   * */
+  public static Fraction dot(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * Cross product
+   * */
+  public static Vector cross(Vector a, Vector b) {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * Zero vector
+   * */
+  public static Vector zero() {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * (1, 0, 0)
+   * */
+  public static Vector eX() {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * (0, 1, 0)
+   * */
+  public static Vector eY() {
+    //TODO stub
+    return null;
+  }
+
+  /**
+   * (0, 0, 1)
+   * */
+  public static Vector eZ() {
+    //TODO stub
+    return null;
+  }
+
+  public static boolean equals(Vector a, Vector b) {
+    //TODO stub
+    return false;
+  }
 
 }
